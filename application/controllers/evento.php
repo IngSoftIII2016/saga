a696c58ef3f0ff8a2914f948aacdd20d9a34c3e2
Sabdro Wainmaier <swainmaier@gmail.com>
2016-09-22 14:55:17 -0300

    abm evento en /evento. evento model que trae todos los eventos de un dia (faltan funciones), eliminacion de index.php del archivo config

--- application/controllers/evento.php
+++ application/controllers/evento.php
@@ -1,33 +1,30 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class evento extends CI_Controller
-{	   
-
-function __construct()
 {
-parent::__construct();
- 
-$this->load->database();
-$this->load->helper('url');
-$this->load->library('grocery_CRUD');
 
-}
- 
- 
-	   public function index()
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->database();
+
+        $this->load->library('grocery_CRUD');
+    }
+
+    public function index()
     {
         $crud = new grocery_CRUD();
 
         $crud->set_table('evento');
-		$crud->set_subject('Evento');
+		$crud->set_relation('Aula_id', 'aula', 'nombre');
 		$crud->display_as('Aula_id','Aula');
-		$crud->set_relation('Aula_id','aula','nombre');
-		$crud->field_type('hora_inicio','time');
-		$crud->field_type('hora_fin','time');
+		$crud->field_type('hora_inicio', 'time');
+		$crud->field_type('hora_fin', 'time');
 		$crud->field_type('motivo', 'text');
         $output = $crud->render();
-		
         $this->load->view('vacia.php', $output);
     }
-	
+
+  
 }
