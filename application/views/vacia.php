8ebfc837e34adfd5046b1fb124278e63d5bc8bf2
fdifabio <fdifabio92@gmail.com>
2016-09-27 18:39:06 -0300

    Vacia.php

--- application/views/vacia.php
+++ application/views/vacia.php
@@ -91,8 +91,38 @@ foreach($css_files as $file): ?>
 <?php endforeach; ?>
 </head>
 <body>
- <?php $this->load->view ( 'header' );?>
+<nav class="navbar navbar-dark" style="background-color: #881300 !important;">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <a class="navbar-brand header" href="#">SAGA</a>
+    </div>
 
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1" style="background-color: #881300 !important;">
+      <ul class="nav navbar-nav navbar-right">
+		<li><a href="#" class="header navclass" style=" font-size: medium;">Buscador</a>
+        </li>
+		<li class="dropdown">
+          <a href="#" class="dropdown-toggle header navclass" style=" font-size: medium;"data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Edificio <span class="caret"></span></a>
+          <ul class="dropdown-menu menudesplegable">
+				<?php
+				foreach($edificios as $edificio):
+				?>
+					<li><a href="#"><?=$edificio->nombre?></a></li>
+				<?php	endforeach; ?>
+          </ul>
+        </li>
+		<li class="dropdown">
+          <a href="#" class="dropdown-toggle header navclass" style=" font-size: medium;"data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Perfil <span class="caret"></span></a>
+          <ul class="dropdown-menu menudesplegable">
+				<li><a href="#">Salir</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
     <div>
 		<?php echo $output; ?>
     </div>
