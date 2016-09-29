ef7defecc2e01cbd1dd020d699bf9b9d6499a7d9
Sabdro Wainmaier <swainmaier@gmail.com>
2016-09-29 12:34:37 -0300

    ctualizacion de controller evento

--- application/controllers/prueba.php
+++ application/controllers/prueba.php
@@ -10,9 +10,10 @@ class prueba extends CI_Controller {
 	
 	public function index() {
 		$this->load->model('Clase_model');
-		//$this->Evento_Model->insertar(13, '2016-09-24', '15:00', '18:00', 'motivo');
+		
 		$this->load->model('Evento_Model');
+		var_dump($this->Evento_Model->agregar_evento(13, '2016-09-24', '15:00', '18:00', 'motivo'));
 		//return $this->Evento_Model->agregar_evento($aula, $fecha, $hora_inicio, $hora_fin, $motivo);
-		var_dump($this->Clase_model->aula_disponible(10, '2016-09-25', '15:00', '18:15'));
+		//var_dump($this->Clase_model->aula_disponible(10, '2016-09-25', '15:00', '18:15'));
 	}
 }
