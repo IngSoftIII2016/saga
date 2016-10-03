0a2006228ba954cf9bfd041c6162cd1fb0cbfc86
sandro wainmaier <sandro wainmaier>
2016-10-03 12:43:53 -0300

    model evento actualizado

--- application/controllers/prueba.php
+++ application/controllers/prueba.php
@@ -12,8 +12,8 @@ class prueba extends CI_Controller {
 		$this->load->model('Clase_model');
 		
 		$this->load->model('Evento_Model');
-		var_dump($this->Evento_Model->agregar_evento(13, '2016-09-24', '15:00', '18:00', 'motivo'));
+		var_dump($this->Evento_Model->aula_disponible_evento(2, '2016-10-03', '08:00', '9:00'));
 		//return $this->Evento_Model->agregar_evento($aula, $fecha, $hora_inicio, $hora_fin, $motivo);
-		//var_dump($this->Clase_model->aula_disponible(10, '2016-09-25', '15:00', '18:15'));
+		//var_dump($this->Clase_model->aula_disponible(10, '2016-09-25', '15:00', '18:00'));
 	}
 }
