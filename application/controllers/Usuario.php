13df76d6858270696ddba43531927a5092724592
erelmuan <eliasrelmuan@gmail.com>
2016-10-10 20:58:58 -0300

    cambiar contrasela para cada usuario

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -12,7 +12,8 @@ class Usuario extends CI_Controller
 		$this->load->library(array('ion_auth','form_validation'));
 		$this->load->helper(array('url','language'));
 		$this->load->library('session');
-
+        $this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+        $this->lang->load('auth');
         $this->load->library('grocery_CRUD');
     }
 
@@ -30,10 +31,10 @@ class Usuario extends CI_Controller
 		$crud->columns('username','email','active','grupos');
 
 		// para editar
-		
+
 		$user_id = $this->session->userdata('user_id');
 
-		 
+
 		if ($user_id ==$this->uri->segment(4)){
 			$crud->edit_fields('first_name', 'last_name','phone','grupos');
 		}
@@ -151,8 +152,85 @@ class Usuario extends CI_Controller
             return true;
         }
     }
-	 
 
+    public  function cambiar_contrasena(){
+        $this->data['body'] = 'cambiar_contrasena';
+        $this->data['data'] = "";
+        $this->form_validation->set_rules('old', $this->lang->line('change_password_validation_old_password_label'), 'required');
+        $this->form_validation->set_rules('new', $this->lang->line('change_password_validation_new_password_label'), 'required|min_length[' . $this->config->item('min_password_length', 'ion_auth') . ']|max_length[' . $this->config->item('max_password_length', 'ion_auth') . ']|matches[new_confirm]');
+        $this->form_validation->set_rules('new_confirm', $this->lang->line('change_password_validation_new_password_confirm_label'), 'required');
+
+        if (!$this->ion_auth->logged_in())
+        {
+            redirect('login/index', 'refresh');
+        }
+
+        $user = $this->ion_auth->user()->row();
+
+        if ($this->form_validation->run() == false)
+        {
+            //display the form
+            //set the flash data error message if there is one
+            $this->data['message'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
+
+            $this->data['min_password_length'] = $this->config->item('min_password_length', 'ion_auth');
+            $this->data['old_password'] = array(
+                'name' => 'old',
+                'id'   => 'old',
+                'type' => 'password',
+            );
+            $this->data['new_password'] = array(
+                'name' => 'new',
+                'id'   => 'new',
+                'type' => 'password',
+                'pattern' => '^.{'.$this->data['min_password_length'].'}.*$',
+            );
+            $this->data['new_password_confirm'] = array(
+                'name' => 'new_confirm',
+                'id'   => 'new_confirm',
+                'type' => 'password',
+                'pattern' => '^.{'.$this->data['min_password_length'].'}.*$',
+            );
+            $this->data['user_id'] = array(
+                'name'  => 'user_id',
+                'id'    => 'user_id',
+                'type'  => 'hidden',
+                'value' => $user->id,
+            );
+
+            //render
+            $this->load->view('templates/base',  $this->data);
+
+          //  $this->_render_page('cambiar_contrasena', $this->data);
+        }
+        else
+        {
+            $identity = $this->session->userdata('identity');
+
+            $change = $this->ion_auth->change_password($identity, $this->input->post('old'), $this->input->post('new'));
+
+            if ($change)
+            {
+                //if the password was successfully changed
+                $this->session->set_flashdata('message', $this->ion_auth->messages());
+               // $this->logout();
+                redirect('usuario/cambiar_contrasena', 'refresh');
+
+            }
+            else
+            {
+                $this->session->set_flashdata('message', $this->ion_auth->errors());
+                redirect('usuario/cambiar_contrasena', 'refresh');
+            }
+        }
+
+    }
+    function _render_page($view, $data=null, $render=false)
+    {
+        $this->viewdata = (empty($data)) ? $this->data: $data;
+        $view_html = $this->load->view($view, $this->viewdata, $render);
+        if (!$render) return $view_html;
+    }
 	
 
 }
