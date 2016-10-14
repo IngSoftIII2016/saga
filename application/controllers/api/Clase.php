4cf99168840f277dee9df1ddc2cd25043a86eed7
Juan Pablo Aveggio <jpaveggio@gmail.com>
2016-10-14 14:00:26 -0300

    intento de url amigable en api rest

--- application/controllers/api/Clase.php
+++ application/controllers/api/Clase.php
@@ -25,12 +25,29 @@ class Clase extends REST_Controller {
 
     }
 
-    public function index_get()
-    {
-    	$this->load->model('Clase_model');
-    	$clases = $this->Clase_model->get_clases();
-    	$this->response(['data' => $clases]);
+    public function index($id){
+        var_dump($id);
     }
+
+
+
+    public function index_get($id)
+    {
+        $this->response(['data' => $this->get()]);
+        die();
+
+        if(isset($id)){
+            $this->load->model('Clase_model');
+            $clase = $this->Clase_model->get_clase($id);
+            $this->response(['data' => $clase]);
+        }else {
+            $this->load->model('Clase_model');
+            $clases = $this->Clase_model->get_clases();
+            $this->response(['data' => $clases]);
+        }
+    }
+
+
     
     
 
