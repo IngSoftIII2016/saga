dfeb7b28556a364b38d3cb1a0b58fc501c1a8370
erelmuan <eliasrelmuan@gmail.com>
2016-10-18 19:34:03 -0300

    implementacion del login en api/usuario, modificacion gestion sesion

---
+++ application/controllers/api/Usuario.php
@@ -0,0 +1,62 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+require APPPATH . '/libraries/REST_Controller.php';
+
+/**
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @author          Phil Sturgeon, Chris Kacerguis
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ */
+class Usuario extends REST_Controller {
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        $this->load->model('Login_Model');
+		$this->load->library(array('ion_auth','form_validation'));
+
+    }
+
+    public function index($id){
+        var_dump($id);
+    }
+
+
+
+    public function login_post ()
+    {
+        $json = $this->post('data');
+        $usuario = $json['usuario'];
+        $contraseña = $json['contraseña'];
+        $recordar= $json['recordar'];
+
+        if ($this->Login_Model->login($usuario, $contraseña, $recordar))
+        {
+            //$cookie=$this->input->cookie('ci_session');
+            $this->response(array('status' => 'success'));
+            //redirect(base_url('planilla'));
+        }
+        else
+            $this->response(array('status' => 'failed'));
+    }
+
+    public function logout_delete ()
+    {
+        $this->ion_auth->logout();
+        $this->response(array('status' => TRUE,'message' => 'ci_session deleted'));
+
+    }
+
+
+
+}
--- application/hooks/Gestion_sesion.php
+++ application/hooks/Gestion_sesion.php
@@ -1,40 +1,41 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 class Gestion_sesion {
-	private $ci;
- public function __construct()
- {
- $this->ci =& get_instance();
- !$this->ci->load->library(array('ion_auth','form_validation')) ? $this->ci->load->library(array('ion_auth','form_validation')) : false;
- }
+    private $ci;
+    public function __construct()
+    {
+        $this->ci =& get_instance();
+        !$this->ci->load->library(array('ion_auth','form_validation')) ? $this->ci->load->library(array('ion_auth','form_validation')) : false;
+    }
 
-   function index(){
+    function index(){
 
-	 if(!$this->ci->ion_auth->logged_in() && $this->ci->uri->segment(1) != 'login'){
-        	redirect(base_url('login'));
+        if(!$this->ci->ion_auth->logged_in() && $this->ci->uri->segment(1) != 'login'){
+            if ($this->ci->uri->uri_string() != 'api/usuario/login')
+                redirect(base_url('login'));
         }
-		 elseif ($this->ci->ion_auth->logged_in() ){
-				$user = $this->ci->ion_auth->user()->row();
-				$user_active = $user->active;
-		
-				if ($user_active==0){
-					$this->ci->session->set_flashdata('message', $this->ci->ion_auth->messages());
-					$logout = $this->ci->ion_auth->logout();
-					redirect('login', 'refresh');
-				}	
+        elseif ($this->ci->ion_auth->logged_in() ){
+            $user = $this->ci->ion_auth->user()->row();
+            $user_active = $user->active;
 
-				if(!$this->ci->ion_auth->is_admin()) {  
-				    //El acceso permitido para el bedel.
-					$controlador = $this->ci->router->class;
-					$controladores_permitidos = array('planilla','horario','login','edificio', 'api/user');
+            if ($user_active==0){
+                $this->ci->session->set_flashdata('message', $this->ci->ion_auth->messages());
+                $logout = $this->ci->ion_auth->logout();
+                redirect('login', 'refresh');
+            }
 
-			    if(!in_array($controlador,$controladores_permitidos)){
-			        if ($this->ci->router->method!=="cambiar_contrasena"){
-					$message='Usted debe ser administrador para acceder a esta pagina. <a href="'.$this->ci->config->config['base_url'].'" >Ir a la planilla</a>';
-					return show_error($message,500, $heading = 'Restricción de acceso');
+            if(!$this->ci->ion_auth->is_admin()) {
+                //El acceso permitido para el bedel.
+                $controlador = $this->ci->router->class;
+                $controladores_permitidos = array('planilla','horario','login','edificio');
+
+                if(!in_array($controlador,$controladores_permitidos)){
+                    if ($this->ci->router->method!=="cambiar_contrasena" && $this->ci->router->method!=="logout"){
+                        $message= $controlador.$this->ci->router->method.'Usted debe ser administrador para acceder a esta pagina. <a href="'.$this->ci->config->config['base_url'].'" >Ir a la planilla</a>';
+                        return show_error($message,500, $heading = 'Restricción de acceso');
                     }
-				 }
-			}
-		}
+                }
+            }
+        }
 
-   }
+    }
 }
