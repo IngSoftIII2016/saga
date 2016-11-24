12cef0a4edfec1998bb5405162612ff120749813
fdifabio <fdifabio92@gmail.com>
2016-11-24 12:30:36 -0300

    Corecciones

--- application/models/ClaseDAO.php
+++ application/models/ClaseDAO.php
@@ -63,7 +63,7 @@ class ClaseDAO extends BaseDAO
     {
        return $this->query(
             [
-                'id !=' => $clase->id,
+                'clase_.id !=' => $clase->id,
                 'fecha' => $clase->fecha,
                 'aula.id' => $clase->aula->id,
                 'hora_inicio <' => $clase->hora_fin,
