8aee87a05920607d92a4ae97501fb359570cb45d
Juan Pablo Aveggio <jpaveggio@gmail.com>
2017-01-10 19:07:54 -0300

    Se acomoda script

--- sql/permisos_roles.sql
+++ sql/permisos_roles.sql
@@ -1,22 +1,5 @@
 USE gestion_aulas;
--- -----------------------------------------------------
--- Table `gestion_aulas`.`ROL`
--- -----------------------------------------------------
 
-CREATE  TABLE `gestion_aulas`.`rol` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `nombre` VARCHAR(45) NULL ,
-  PRIMARY KEY (`id`) )
-  engine = InnoDB;
-
--- -----------------------------------------------------
--- INSERT Table `gestion_aulas`.`rol`
--- -----------------------------------------------------
-
-INSERT INTO `gestion_aulas`.`rol` (`nombre`) VALUES ('admin');
-INSERT INTO `gestion_aulas`.`rol` (`nombre`) VALUES ('administracion academica');
-INSERT INTO `gestion_aulas`.`rol` (`nombre`) VALUES ('bedel');
-INSERT INTO `gestion_aulas`.`rol` (`nombre`) VALUES ('alumno');
 
 -- -----------------------------------------------------
 -- INSERT Table `Usuario`
@@ -37,16 +20,6 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'$2y$08$GjNYMBqLQ5e08LQtG5R/WOpK51JYTkkQZJal3Tj9F96dxWot.QqxK','administrador',1,'administrador','saga','administrador');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-
 -- -----------------------------------------------------
 -- COLUMN `rol` Table `gestion_aulas`.`usuario`
 -- -----------------------------------------------------
@@ -54,12 +27,33 @@ UNLOCK TABLES;
 ALTER TABLE `gestion_aulas`.`usuario` ADD COLUMN `rol` INT NOT NULL AFTER `nombre_usuario`;
 
 -- -----------------------------------------------------
--- UPDATE roles a usuarios
+-- Table `gestion_aulas`.`ROL`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `gestion_aulas`.`rol`;
+CREATE TABLE `gestion_aulas`.`rol` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `nombre` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id`) )
+  engine = InnoDB;
+
+-- -----------------------------------------------------
+-- INSERT Table `gestion_aulas`.`rol`
 -- -----------------------------------------------------
 
-UPDATE `gestion_aulas`.`usuario` SET `rol`='1' WHERE `id`='1';
-UPDATE `gestion_aulas`.`usuario` SET `rol`='2' WHERE `id`='24';
-UPDATE `gestion_aulas`.`usuario` SET `rol`='3' WHERE `id`='25';
+INSERT INTO `gestion_aulas`.`rol` (`nombre`) VALUES ('admin');
+INSERT INTO `gestion_aulas`.`rol` (`nombre`) VALUES ('administracion academica');
+INSERT INTO `gestion_aulas`.`rol` (`nombre`) VALUES ('bedel');
+INSERT INTO `gestion_aulas`.`rol` (`nombre`) VALUES ('alumno');
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'$2y$08$GjNYMBqLQ5e08LQtG5R/WOpK51JYTkkQZJal3Tj9F96dxWot.QqxK','administrador',1,'administrador','saga','administrador', 'Administrador', 1);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
 
 -- -----------------------------------------------------
 -- FOREIGN KEY `rol` Table `gestion_aulas`.`usuario`
@@ -76,7 +70,7 @@ ALTER TABLE `gestion_aulas`.`usuario`
 -- -----------------------------------------------------
 -- Table `gestion_aulas`.`accion`
 -- -----------------------------------------------------
-
+DROP TABLE IF EXISTS `gestion_aulas`.`accion`;
 CREATE TABLE `gestion_aulas`.`accion` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `url` VARCHAR(100) NULL ,
@@ -87,7 +81,7 @@ CREATE TABLE `gestion_aulas`.`accion` (
 -- -----------------------------------------------------
 -- Table `gestion_aulas`.`accion_rol`
 -- -----------------------------------------------------
-
+DROP TABLE IF EXISTS `gestion_aulas`.`accion_rol`
 CREATE  TABLE `gestion_aulas`.`accion_rol` (
   `Accion_id` INT NULL ,
   `Rol_id` INT NULL )
@@ -116,4 +110,4 @@ ALTER TABLE `gestion_aulas`.`accion_rol`
 -- COLUMN `recurso` Table `gestion_aulas`.`accion`
 -- -----------------------------------------------------
 
-ALTER TABLE `gestion_aulas`.`accion` ADD COLUMN `recurso` VARCHAR(45) NULL  AFTER `metodo` ;
+ALTER TABLE `gestion_aulas`.`accion` ADD COLUMN `recurso` VARCHAR(45) NULL  AFTER `metodo`;
