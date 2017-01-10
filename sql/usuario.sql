4a99d92124771e6a01d3c9799b7e7f4c3b2474ad
JavierJV <valsecchi_javier@hotmail.com>
2017-01-10 18:17:42 -0300

    usuario sql

--- sql/usuario.sql
+++ sql/usuario.sql
@@ -1,77 +1,45 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 05-12-2016 a las 16:55:37
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 5.6.23
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: gestion_aulas
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.16-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `gestion_aulas`
+-- Table structure for table `usuario`
 --
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
-
-DELETE * FROM usuario_grupo;
-
-DROP TABLE usuario;
-
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `nombre_usuario` varchar(100) NOT NULL,
-  `contraseña` varchar(255) NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `estado` tinyint(1) UNSIGNED DEFAULT NULL,
+  `estado` tinyint(1) unsigned DEFAULT NULL,
   `nombre` varchar(50) DEFAULT NULL,
   `apellido` varchar(50) DEFAULT NULL,
-  `telefono` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nombre_usuario` (`nombre_usuario`),
-  ADD UNIQUE KEY `email` (`email`);
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`id`, `nombre_usuario`, `contraseña`, `email`, `estado`, `nombre`, `apellido`, `telefono`) VALUES
-(1, 'administrador', '$2y$08$GjNYMBqLQ5e08LQtG5R/WOpK51JYTkkQZJal3Tj9F96dxWot.QqxK', 'admin@admin.com', 1, 'Elias', 'Relmuan', '43432423'),
-(24, 'juan perez', '$2y$08$QHYT4v4M60DOvZ5THflEjeMx2VHRip4pvD3giR6SirGHc4x3NxYCu', 'juan@otulook.es', 0, 'juan', 'perezs', '555555521'),
-(25, 'argento', '$2y$10$RDrWUYJqIvfy/uN3ia6g7OyIe8JGQVlU4GQQiDApQM1dJ2Xv3hcqq', 'esteabotero@gmail.com', 1, 'andres', 'gonzales', '23123123');
---
--- Índices para tablas volcadas
---
+  `nombre_usuario` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
-
-
---
--- AUTO_INCREMENT de las tablas volcadas
+-- Dumping data for table `usuario`
 --
 
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'$2y$08$GjNYMBqLQ5e08LQtG5R/WOpK51JYTkkQZJal3Tj9F96dxWot.QqxK','administrador',1,'administrador','saga','administrador');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
