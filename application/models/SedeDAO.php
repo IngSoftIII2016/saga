e98716e3b98709463d070875d535a500f400d625
JavierJV <valsecchi_javier@hotmail.com>
2017-02-06 10:56:48 -0300

    Alta Sede - correjido el problema al validar que el campo nombre no estubiera vacio.

--- application/models/SedeDAO.php
+++ application/models/SedeDAO.php
@@ -5,17 +5,25 @@ class SedeDAO extends BaseDAO {
 	public function __construct() {
 		parent::__construct ( 'Sede' );
 	}
-	
+
+    protected function is_invalid($entity)
+    {
+
+        if ((strlen($entity->nombre)) <= 0) {
+            return format_error('Faltan Campos Requeridos','faltan campos requeridos', 500);
+        }else
+            return FALSE;
+    }
 	/**
 	 *
 	 * @param $entity
 	 * @return array|bool
 	 */
 	protected function is_invalid_insert($entity){
-		return FALSE;
+		return $this->is_invalid($entity);
 	}
 	protected function is_invalid_update($entity){
-		return FALSE;
+		return $this->is_invalid($entity);
 	}
 	protected function is_invalid_delete($entity){
 		return FALSE;
