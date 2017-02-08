9207d9e75edc6a6a74e92fc12953dfc61da4a205
Juan Pablo Aveggio <jpaveggio@gmail.com>
2017-02-08 16:29:01 -0300

    Validacion de campos vacios en DAO's

--- application/models/RolDAO.php
+++ application/models/RolDAO.php
@@ -1,9 +1,11 @@
 <?php
 require_once APPPATH . '/models/orm/BaseDAO.php';
 
-class RolDAO extends BaseDAO {
+class RolDAO extends BaseDAO
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct('Rol');
     }
 
@@ -11,13 +13,27 @@ class RolDAO extends BaseDAO {
      * @param $entity
      * @return array|bool
      */
-    protected function is_invalid_insert($entity){
+
+    private function is_invalid($entity)
+    {
+        if (validate_not_empty($entity->nombre) == NULL) {
+            return format_error('Campo Faltante', 'el campo nombre es obligatorio');
+        }
         return FALSE;
     }
-    protected function is_invalid_update($entity){
-        return FALSE;
+
+    protected function is_invalid_insert($entity)
+    {
+        return $this->is_invalid($entity);
     }
-    protected function is_invalid_delete($entity){
+
+    protected function is_invalid_update($entity)
+    {
+        return  $this->is_invalid($entity);
+    }
+
+    protected function is_invalid_delete($entity)
+    {
         return FALSE;
     }
 }
