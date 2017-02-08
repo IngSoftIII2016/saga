25184f15067107ff5ff878ddccbf55c1b7e1e325
JavierJV <valsecchi_javier@hotmail.com>
2017-02-08 16:54:19 -0300

    AuthEndpoint - Limapiado codigo duplicado

--- application/controllers/api/AuthEndpoint.php
+++ application/controllers/api/AuthEndpoint.php
@@ -131,27 +131,6 @@ class AuthEndpoint extends BaseEndpoint
         }
     }
 
-
-    function get_random_password($chars_min = 6, $chars_max = 8, $use_upper_case = true, $include_numbers = true, $include_special_chars = true)
-    {
-        $length = rand($chars_min, $chars_max);
-        $selection = 'aeuoyibcdfghjklmnpqrstvwxz';
-        if ($include_numbers) {
-            $selection .= "1234567890";
-        }
-        if ($include_special_chars) {
-            $selection .= "@#$?";
-        }
-
-        $password = "";
-        for ($i = 0; $i < $length; $i++) {
-            $current_letter = $use_upper_case ? (rand(0, 1) ? strtoupper($selection[(rand() % strlen($selection))]) : $selection[(rand() % strlen($selection))]) : $selection[(rand() % strlen($selection))];
-            $password .= $current_letter;
-        }
-
-        return $password;
-    }
-
     private static function Aud()
     {
         $aud = '';
